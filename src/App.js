cefeca3e66b4efa65eb5ad82dd71b24e26d4b10e
Jeevan Gurung <53844739+G1Grg@users.noreply.github.com>
2021-06-09 17:53:38 +1000

    learning about components
    Signed-off-by: Jeevan Gurung <53844739+G1Grg@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Header(){
+  return(
+    <header>
+      <h1>This is eve's kitchen</h1>
+    </header>
+  )
+}
+
+function Main(){
+  return (
+    <section>
+      <h2><p>We serve delicious food</p></h2>
+    </section>
+  )
+}
+
+function Footer(){
+  return (
+    <footer>
+      <h3><p>we are d.....</p></h3>
+    </footer>
+  )
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Main/>
+      <Footer/>
     </div>
   );
 }
