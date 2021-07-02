3790d5d91cec7e202db3de63f5aa6971c97e3985
Jeevan Gurung <53844739+G1Grg@users.noreply.github.com>
2021-07-02 16:01:00 +1000

    useReducer Hook insteadof useState
    Signed-off-by: Jeevan Gurung <53844739+G1Grg@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React, {useState} from "react";
+import React, {useState, useReducer} from "react";
 import './App.css';
 
 // useState hook
 
 function App() {
   const [emotion, setEmotion] = useState("Happy");
+  const [checked, toggle] = useReducer(
+    (checked) => !checked,
+    false
+    );
   
+   
   return (
   <>
   <h1>I am in a {emotion} state</h1>
@@ -19,6 +24,13 @@ function App() {
      Sad
    </button>
 
+  <h1>I am using useReducer Hook</h1>
+  <input type ="checkbox"
+    value ={checked}
+    onChange={toggle}/>
+  <p>{checked ? "checked" : "not checked" }</p>
+  
+  
   </>
   )
 }
