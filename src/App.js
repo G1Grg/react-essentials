dffd5e23543fa16c80092bdf38066089519cd470
Jeevan Gurung <53844739+G1Grg@users.noreply.github.com>
2021-07-02 15:51:14 +1000

    use of useSate Hook
    Signed-off-by: Jeevan Gurung <53844739+G1Grg@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,57 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import './App.css';
-import table from './table.jpg'
 
-const dishes =[
-  "Food 1",
-  "food 2",
-  "food 3",
-  "food 4",
-  "food 5"
-]
+// useState hook
 
-const dishesObject = dishes.map((dish,i)=>({id: i, title: dish}));
-
-function Header(props){
-  return(
-    <header>
-      <h1>This is {props.name}'s kitchen</h1>
-    </header>
-  )
-}
-
-function Main(props){
-  return (
-    <section>
-      <h2><p>We serve {props.a} food</p></h2>
-      <img 
-      src={table} height={200} alt ="table for four"></img>
-      <ul style ={{textAlign:"Left" }}>
-        {props.dish.map((dishing)=>
-        <li key={dishing.id}>
-          {dishing.title}
-        </li>)}
-      </ul>
-    </section>
-  )
-}
-
-
-function Footer(props){
-  return (
-    <footer>
-      <h3><p>copyright {props.year}</p></h3>
-    </footer>
-  )
-}
 function App() {
+  const [emotion, setEmotion] = useState("Happy");
+  
   return (
-    <div className="App">
-      <Header name="Jeevan" />
-      <Main text="amazing" dish ={dishesObject} />
-      <Footer year={new Date().getFullYear()} />
-    </div>
-  );
+  <>
+  <h1>I am in a {emotion} state</h1>
+   <button onClick ={() => setEmotion("Grumpy")}>
+     Grumpy
+   </button>
+   <button onClick ={() => setEmotion("rude")}>
+     Rude
+   </button>
+   <button onClick ={() => setEmotion("sad")}>
+     Sad
+   </button>
+
+  </>
+  )
 }
 
 export default App;
